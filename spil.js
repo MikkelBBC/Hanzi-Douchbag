229d4bcc6748a1aaa8854120c169ffca5258a474
MikkelBBC <mikkel.hoimark@gmail.com>
2024-10-14 20:27:57 +0100

    Lidt mere

--- spil.js
+++ spil.js
@@ -4,15 +4,19 @@ function KøbStort() {
 
 
     
+ if (Penge >= 10) {
+
+    penges = Penge - 10;
 
     window.parent.globalValues.købTeks += 1;
     window.parent.opdaterTekst('købTeks', window.parent.globalValues.købTeks + " Styrke");
     document.getElementById('købTeks').textContent = window.parent.globalValues.købTeks + " Styrke";
 
+ }
+   
 
-    window.parent.globalValues.købTeks += 1;
-    window.parent.opdaterTekst('købTeks', window.parent.globalValues.købTeks + " Styrke");
-    document.getElementById('købTeks').textContent = window.parent.globalValues.købTeks + " Styrke";
+
+   
 }
 
 // Når iframe indlæses, skal vi initialisere værdierne fra window.parent
